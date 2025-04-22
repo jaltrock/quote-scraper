5d40d6c93fd2011f49316e40ca89ebd9c3e4c67d
jaltrock <altrockweb@gmail.com>
2025-04-22 15:50:40 -0400

    Update API URL for production

--- practical-guide-quotes-frontend/src/app/page.tsx
+++ practical-guide-quotes-frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ type Quote = {
 };
 
 export default async function HomePage() {
-  const res = await fetch("http://127.0.0.1:8000/quotes");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/quotes`);
   const data: Quote[] = await res.json();
 
 
