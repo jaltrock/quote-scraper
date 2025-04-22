f37a618356a4520e9b3a7b68a43bb392fb81beac
jaltrock <altrockweb@gmail.com>
2025-04-22 15:18:13 -0400

    Update API URL for production

--- practical-guide-quotes-frontend/src/app/page.tsx
+++ practical-guide-quotes-frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ type Quote = {
 };
 
 export default async function HomePage() {
-  const res = await fetch("http://127.0.0.1:8000/quotes");
+  const res = await fetch("https://practical-guide-api.onrender.com");
   const data: Quote[] = await res.json();
 
 
